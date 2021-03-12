fea6f3bdc466807aabe89ee367845ec0725031db
Alejandro Garcia Gallardo <77563527+AlejandroGaGa@users.noreply.github.com>
2021-03-12 16:41:45 -0600

    changes for v3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import  Amplify from 'aws-amplify';
+import awsconfig from '/aws-exports';
+import {AmplifySignOut, withAuthenticator} from '@aws-amplify/ui-react';
+
+Amplify.configure(awsconfig);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Mi app de react implementada en la nube de AWS con conexión a Github :)</h1>
+        <AmplifySignOut />
+        <h2>My App Content</h2>
       </header>
     </div>
   );
 }
 
-export default App;
+
+export default withAuthenticator(App);
